e7d850b41713d52d4a1f751c965ec27bf7215112
Rasmus-Emil Hansen <r_emil@live.dk>
2021-01-18 11:08:40 +0100

    like logic, hasManyThrough relationship to count total like on 1 persons posts

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,7 +3,28 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            {{ $user->name }}
+            <div>
+                <h1 class="text-2xl font-medium mb-1">
+                    {{ $user->name }}
+                </h1>
+                <p class="mb-2">
+                    Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}
+                </p>
+                <p class="mb-2">
+                    Received {{ $user->receivedLikes->count() }} likes
+                </p>
+                <hr class="mb-2">
+            </div>
+            @if ($posts->count() > 0)
+                @foreach($posts as $post)
+                    <x-post :post="$post" />
+                @endforeach
+                {{ $posts->links() }}
+            @else
+                <p>
+                    {{ $user->name }} does not have any posts
+                </p>
+            @endif
         </div>
     </div>
 @endsection
